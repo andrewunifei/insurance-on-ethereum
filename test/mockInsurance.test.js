a08c139b6a9909b91687077ce391d8f6ba26a7c9
Andrew <andrewunifei@gmail.com>
2024-02-07 17:46:44 -0300

    [ADD] Test

--- test/mockInsurance.test.js
+++ test/mockInsurance.test.js
@@ -35,7 +35,6 @@ describe('mockAutomatedFunctionsConsumer', async () => {
         );
         await insuranceContract.deployTransaction.wait(1);
 
-
         insuranceContract.institution()
             .then(value => expect(value).to.equal('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'))
 
